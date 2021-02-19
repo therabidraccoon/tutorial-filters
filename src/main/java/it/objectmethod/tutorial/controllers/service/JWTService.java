ab8e181f81bd7e631a7f96b995cfc549604292ef
Ivan Maltese <ivan.maltese@objectmethod.it>
2021-02-19 16:50:33 +0100

    fake jwt service

---
+++ src/main/java/it/objectmethod/tutorial/controllers/service/JWTService.java
@@ -0,0 +1,15 @@
+package it.objectmethod.tutorial.controllers.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTService {
+
+	public String createJWTToken() {
+		return "";
+	}
+
+	public boolean checkJWTToken() {
+		return true;
+	}
+}
